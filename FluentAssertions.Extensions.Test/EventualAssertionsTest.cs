bfd62a3ff426daae423cc95b9fe80438311a42d4
Kittyfisto <kittyfisto87@gmail.com>
2017-04-01 22:53:37 +0200

    Added tests

---
+++ FluentAssertions.Extensions.Test/EventualAssertionsTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace FluentAssertions.Extensions.Test
+{
+	[TestFixture]
+	public sealed class EventualAssertionsTest
+	{
+		[Test]
+		public void TestShouldEventuallyBeTrue1()
+		{
+			var obj = new TestClass1 {IsFinished = true};
+			obj.Property(x => x.IsFinished).ShouldEventually().BeTrue();
+		}
+
+		[Test]
+		public void TestShouldEventuallyBeTrue2()
+		{
+			var obj = new TestClass1 { IsFinished = false };
+			obj.Property(x => x.IsFinished).ShouldEventually().BeFalse();
+		}
+	}
+}
---
+++ FluentAssertions.Extensions.Test/TestClass1.cs
@@ -0,0 +1,7 @@
+namespace FluentAssertions.Extensions.Test
+{
+	public sealed class TestClass1
+	{
+		public bool IsFinished { get; set; }
+	}
+}
